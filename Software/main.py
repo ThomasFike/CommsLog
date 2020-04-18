b5cfebeca770c6f103226fc66d27868dc7a538ac
Thomas Fike <Fike@me.com>
2020-04-18 16:04:44 -0700

    Adding Contact Entry

--- Software/main.py
+++ Software/main.py
@@ -1,24 +1,35 @@
 
+from tinydb import TinyDB, Query
+import time
+
+
 class Contact:
     def __init__(self):
         self.time = 0
         self.transmiting = ""
         self.receiving = ""
         self.message = ""
-    def log(self, db):
-        db.insert({'time': self.time, 'Tx': self.transmiting, 'Rx': self.receiving, 'msg': self.message})
 
-from tinydb import TinyDB, Query
-import time
+    def log(self, db):
+        db.insert({'time': self.time, 'Tx': self.transmiting,
+                   'Rx': self.receiving, 'msg': self.message})
+
+    def getContactInfo(self):
+        self.transmiting = input("Station: ")
+        self.time = time.time()
+        self.message = input("Message: ")
+
+
 db = TinyDB('./test.json')
 db.purge()
 contact = Contact()
-contact.time = time.time()
-contact.receiving = "NC"
-contact.transmiting = "Jackass Junction"
-contact.message = "Hello"
-contact.log(db)
-
 User = Query()
-print(db.all())
+contact.receiving = "NC"
+while (input("Continue:").lower() == "y"):
+    contact.getContactInfo()
+    contact.log(db)
 
+print(db.all())
+last = db.get(doc_id=len(db))
+print("Last contact:")
+print(f"Rx: {last[")
