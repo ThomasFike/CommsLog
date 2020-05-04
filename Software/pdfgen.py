44725531a2fce77d60c900dfa68faa82cd38aa45
Thomas Fike <fike@me.com>
2020-05-03 19:50:44 -0700

    Finished PDF Gen

--- Software/pdfgen.py
+++ Software/pdfgen.py
@@ -1,8 +1,7 @@
 from fpdf import FPDF
-from tinydb import TinyDB, Query
+from tinydb import TinyDB
 import time
 
-db = TinyDB('Database.json')
 space = '                                             '
 field8 = '8. Prepared by:    Name:' + space + 'Position/Title:' + space + 'Signature:\n'
 class PDF(FPDF):
@@ -16,11 +15,8 @@ class PDF(FPDF):
         self.multi_cell(0, pdf.font_size*1.5, field8 + 'ICS 309, Page ' + str(self.page_no() ) + space + space[0:13] +'Date/Time:', border=1, align='L', fill=False)
 
 pdf = PDF(orientation='P', unit='in', format='letter')
-pdf.alias_nb_pages()
-pdf.add_page()
-pdf.set_font("Arial", size=12)
 
-def Header(spacing = 1):
+def Header(incident, OpStart, OpEnd, NetName, RADO, spacing = 1):
     col_width = pdf.w / 4.5
     row_height = pdf.font_size
 
@@ -30,26 +26,26 @@ def Header(spacing = 1):
     pdf.ln(-row_height * spacing * 3)
     pdf.set_x(pdf.w/6.5 + pdf.get_x())
     pdf.multi_cell(pdf.w/3.5, row_height * spacing * 1.5,
-                txt='1. Incident Name\n Luke Flyover Observer Net',
+                txt='1. Incident Name\n' + incident,
                 border=1, align='L')
     pdf.ln(-row_height * spacing * 3)
     pdf.set_x(pdf.w/6.5 + pdf.get_x() + pdf.w/ 3.5)
     pdf.multi_cell(0, row_height * spacing * 1.5,
-                txt='2. Operational Period\nFrom: 2020-05-01 15:00  To: 2020-05-01 16:00',
+                txt='2. Operational Period\nFrom: ' + OpStart + ' To: ' + OpEnd,
                 border=1, align='L')
     
     #Row 1
     pdf.multi_cell(pdf.w/2.275, row_height * spacing * 1.5,
-                txt='3. Net Name\nAEN-MAR',
+                txt='3. Net Name\n' + NetName,
                 border=1, align='L')
     pdf.ln(-row_height * spacing * 3)
     pdf.set_x(pdf.w/2.275 + pdf.get_x())
     pdf.multi_cell(0, row_height * spacing * 1.5,
-                txt='4. RADO (Name, Callsign)\nThomas Fike, KG7FXT',
+                txt='4. RADO (Name, Callsign)\n' + RADO,
                 border=1, align='L')
 
 
-def DataTable(spacing=1):
+def DataTable(db, spacing=1):
     data = [['Time', 'Tx', 'Rx', 'Message']]
     for item in db.all():
         data.append(list(item.values()))
@@ -66,22 +62,22 @@ def DataTable(spacing=1):
         pdf.ln(row_height*spacing)
 
 
-def simple_table(spacing=1):
-    data = [['First Name', 'Last Name', 'email', 'zip'],
-            ['Mike', 'Driscoll', 'mike@somewhere.com', '55555'],
-            ['John', 'Doe', 'jdoe@doe.com', '12345'],
-            ['Nina', 'Ma', 'inane@where.com', '54321']
-            ]
+def GeneratePDF(db):
+    #Collect Info
+    print("Welcome to PDF Printer")
+    print("This will write out the contents of your database to a ICS 309")
+    incident = input('Incident Name: ')
+    OpStart = input('Operational Period Start (Date and Time): ')
+    OpEnd = input('Operational Period End (Date and Time): ')
+    NetName = input('Net name: ')
+    RADO = input('RADO info (name and callsign): ')
 
-    col_width = pdf.w / 4.5
-    row_height = pdf.font_size
-    for row in data:
-        for item in row:
-            pdf.cell(col_width, row_height*spacing,
-                     txt=item, border=1)
-        pdf.ln(row_height*spacing)
-
-Header()
-pdf.ln(pdf.font_size)
-DataTable(1.5)
-pdf.output("test.pdf", 'F')
+    #Generating PDF
+    pdf.alias_nb_pages()
+    pdf.add_page()
+    pdf.set_font("Arial", size=12)
+    Header(incident, OpStart, OpEnd, NetName, RADO)
+    pdf.ln(pdf.font_size)
+    DataTable(db, 1.5)
+    pdf.output(incident + '_' + NetName + '.pdf', 'F')
+    print("File Written as " + incident + '_' + NetName + '.pdf')
