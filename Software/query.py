c0732b24f7877f5b8c838efc70f9b5f1f129a533
Thomas Fike <fike@me.com>
2020-05-05 19:35:40 -0700

    added messageContains

--- Software/query.py
+++ Software/query.py
@@ -26,13 +26,32 @@ def Last5Tx(db, User, TxSearch):
     print(t)
     input("Press enter to continue")
 
+def messageContains(db, User, searchTerm):
+    clear()
+    Working = Contact()
+    contacts = db.search(User.msg.search(searchTerm + '*', flags=re.IGNORECASE))
+    if len(contacts) == 0:
+        return
+    t = PrettyTable(['Rx', 'Tx', 'Time', 'Message'])
+    for ithContact in contacts:
+        Working.load(ithContact)
+        t.add_row([Working.receiving, Working.transmitting,
+                       time.ctime(Working.time), Working.message])
+    print(t)
+    input("Press enter to continue")
+
+
 def lookupDialog(db, User):
     print("Welcome to lookup please select and option:")
     print("1. Last 5 Contacts by Tx Station")
+    print("2. Message contains word")
 
     action = input("Option: ")
     if (action.lower() == "1"):
         TxSearch = input("Tx Station: ")
         Last5Tx(db, User, TxSearch)
+    elif (action.lower() == '2'):
+        searchTerm = input('Word: ')
+        messageContains(db, User, searchTerm)
     else:
         pass
